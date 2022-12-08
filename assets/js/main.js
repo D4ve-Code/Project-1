69de49592eb7bda0bf9821c1ea903b261832d02f
Alexandre Essaifi <alex-es-31@outlook.com>
2022-12-08 10:14:58 +0100

    started js function

---
+++ assets/js/main.js
@@ -0,0 +1,73 @@
+// let card = document.getElementById("card");
+// let theCard = document.querySelector(".thecard");
+// let theCardChosen = document.querySelector(".thecard-chosen");
+// let styleMain = document.querySelector(".back-chosen");
+// let styleBack = document.querySelector(".back");
+
+// function change() {
+//   if (card === document.querySelector(".carte1")) {
+//     theCardChosen = true;
+//     styleMain = true;
+//   } else if (card === document.querySelector(".carte2")) {
+//     theCardChosen.classList.replace("thecard-chosen","thecard");
+//     styleMain.classList.replace("back-chosen","back");
+//     theCard.classList.replace("thecard","thecard-chosen");
+//     styleBack.classList.replace("back","back-chosen");
+
+
+
+//   } else if (card === document.querySelector(".carte3")) {
+    
+   
+//   } else if (card === document.querySelector(".carte4")) {
+//     theCard = theCardChosen;
+//     styleBack = styleMain;
+//   } else if (card === document.querySelector(".carte5")) {
+//     theCard = theCardChosen;
+//     styleBack = styleMain;
+//   } else if (card === document.querySelector(".carte6")) {
+//     theCard = theCardChosen;
+//     styleBack = styleMain;
+//   } else if (card === document.querySelector(".carte7")) {
+//     theCard = theCardChosen;
+//     styleBack = styleMain;
+//   }
+// }
+
+// function choosecards() {
+//   let choosecard = Math.floor(Math.random() * 7 + 1);
+
+//   if (choosecard === 1) {
+//     card = document.querySelector(".carte1");
+//     change();
+//     console.log(card);
+//   } else if (choosecard === 2) {
+//     card = document.querySelector(".carte2");
+//     change();
+//     console.log(card);
+//   } else if (choosecard === 3) {
+//     card = document.querySelector(".carte3");
+//     change();
+//     console.log(card);
+//   } else if (choosecard === 4) {
+//     card = document.querySelector(".carte4");
+//     change();
+//     console.log(card);
+//   } else if (choosecard === 5) {
+//     card = document.querySelector(".carte5");
+//     change();
+//     console.log(card);
+//   } else if (choosecard === 6) {
+//     card = document.querySelector(".carte6");
+//     change();
+//     console.log(card);
+//   } else if (choosecard === 7) {
+//     card = document.querySelector(".carte7");
+//     change();
+//     console.log(card);
+//   } else {
+//     console.log("rien");
+//   }
+//   return choosecard;
+// }
+// console.log(choosecards());
